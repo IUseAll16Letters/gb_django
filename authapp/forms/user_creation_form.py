3d7386551d2981d626a005a42ee0de162e9475ec
IUseAll16Letters <ch1ck3n@mail.ru>
2022-12-19 17:48:06 +0300

    Add register form, modify register view

---
+++ authapp/forms/user_creation_form.py
@@ -0,0 +1,32 @@
+__all__ = ['CustomUserCreationForm']
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    field_order = [
+        "username",
+        "password1",
+        "password2",
+        "email",
+        "first_name",
+        "last_name",
+        "age",
+        "avatar",
+    ]
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "age",
+            "avatar",
+        )
+        field_classes = {"username": UsernameField}
